92e4ad00702f1fa0fc02e1ae6c26a46e0ddc030d
methmk <evanmakeyev@gmail.com>
2021-11-12 18:10:15 +0200

    animated

--- index.js
+++ index.js
@@ -1,4 +1,14 @@
+
+
 function PageTransitionOut() {
+    var tl = gsap.timeline()
+    tl.from('div.transition', {duration: .5, opacity:1})
+    tl.to('div.transition', {duration: .5, opacity:0})
+
+}
+
+
+function PageTransitionIn() {
     var tl = gsap.timeline()
     tl.from('div.transition', {duration: .5, opacity:0})
     tl.to('div.transition', {duration: .5, opacity:1})
@@ -6,23 +16,53 @@ function PageTransitionOut() {
 }
 
 
-function PageTransitionIn() {
-    var tl = gsap.timeline()
-    tl.from('div.transition', {duration: .3, opacity:0})
-    tl.to('div.transition', {duration: .3, opacity:1})
-
-}
 
 
-
-
-
-function contentAnimation(){
+function homeAnimation(){
     var tl = gsap.timeline();
-   //home page
-
+    tl.from('div.main-content h2', {duration: 1, opacity:0, x:-50})
+    tl.from('div.main-content p', {duration: 1, opacity:0, x:-50})
+    tl.from('span', {duration: .5, opacity:0})
+    tl.from('div.tech-list li', {duration: .5, opacity:0, y:50, stagger:0.2})
+}
+function devicesAnimation(){
+    var tl = gsap.timeline();
+    tl.from('div.head-content h3', {duration: 1, opacity:0, x:-50})
+    tl.from('div.head-content', {duration: 1, opacity:0, x:-50})
+    tl.from('.sonata', {duration: .5, opacity:0, y:-200, stagger:0.5})
+    tl.from('ul.anim-dev-list li', {duration: .5, opacity:0, stagger:0.3})
+   
 
 }
+function platformAnimation(){
+    var tl = gsap.timeline();
+    tl.from('div.glass', {duration: 1, opacity:0})
+    tl.from('div.serwant-ipad ', {duration: 1,x:-100, opacity:0})
+    tl.from('div.anim-text ', {duration: 1, opacity:0})
+    tl.from('ul.anim-platlist li', {duration: .5, opacity:0, stagger:0.3})
+}
+
+function partnersAnimation(){
+    var tl = gsap.timeline();
+    tl.from('div.anim-head-text', {duration: 1, opacity:0, x:-50})
+    tl.from('div.partners-content p', {duration: 1, opacity:0, x:-50})
+    tl.from('li#stat1', {opacity:0})
+    tl.from('span#country', { innerText: 0, duration: 2, snap: {innerText:1}})
+    tl.from('li#stat2', {opacity:0})
+    tl.from('span#years', { innerText: 0, duration: 2, snap: {innerText:1}})
+    tl.from('li#stat3', {opacity:0})
+    tl.from('ul.anim-ul li', {duration: .5, opacity:0, stagger:0.3})
+    
+    
+}
+function contactsAnimation(){
+    var tl = gsap.timeline();
+    tl.from('div.contacts', {duration: 1, opacity:0, x:-50})
+    tl.from('ul.anim-li li', {duration: .5, opacity:0, stagger:0.3})
+    
+    
+}
+
 
 
 function delay(n) {
@@ -35,34 +75,61 @@ function delay(n) {
 }
 
 barba.init({
-
+    sync: false,
 
     transitions: [{
-        sync: true,
-
-
 
                 leave(data){
-
-
                 PageTransitionOut();
-
                 console.log("leave");
 
 
             },
 
-             enter(data){
+             async enter(data){
                 PageTransitionIn();
                  console.log("enter");
             },
                 
-            afterEnter(data) {          
-                contentAnimation();
-                    console.log("anim");
-                
-              }
 
-        }]
+        }],
+
+        views: [{
+            namespace: 'home',
+            afterEnter(data) {
+                homeAnimation();
+                console.log("animate")
+            }
+          }, 
+          
+          {
+            namespace: 'devices',
+            afterEnter(data) {
+                devicesAnimation();
+                console.log("animate")
+              }
+            },
+            {
+                namespace: 'platform',
+                afterEnter(data) {
+                    platformAnimation();
+                    console.log("animate")
+                  }
+            },
+            {
+                namespace: 'partners',
+                afterEnter(data) {
+                    partnersAnimation();
+                    console.log("animate")
+                  }
+                },
+                {
+                    namespace: 'contacts',
+                    afterEnter(data) {
+                        contactsAnimation();
+                        console.log("animate")
+                      }
+            
+          }], 
     
 })
